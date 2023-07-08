a636948c0283cb4d6d9fbd7cc98a94dc03b62ced
Albert Bou Pinar <alboup@gmail.com>
2023-07-08 14:13:15 +0200

    Exercici 3

--- src/components/StarshipDetails.tsx
+++ src/components/StarshipDetails.tsx
@@ -3,33 +3,54 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 type Starship = {
-  name: string;
-  model: string;
-  // Add other properties as needed
-}
-
-const StarshipDetails: React.FC = () => {
-  const { id } = useParams();
-  const [starship, setStarship] = useState<Starship | null>(null);
-
-  useEffect(() => {
-    axios.get(`https://swapi.dev/api/starships/${id}/`)
-      .then(response => {
-        setStarship(response.data);
-      })
-      .catch(error => console.log(error));
-  }, [id]);
-
-  if (!starship) return <p>Loading...</p>;
-
-  return (
-    <div>
-      <h2>{starship.name}</h2>
-      <p>{starship.model}</p>
-      {/* Show other properties as needed */}
-      <img src={`https://starwars-visualguide.com/assets/img/starships/${id}.jpg`} alt={starship.name} />
-    </div>
-  );
-};
-
-export default StarshipDetails;
+    name: string;
+    model: string;
+    starship_class: string;
+    manufacturer: string;
+    cost_in_credits: string;
+    crew: string;
+    passengers: string;
+    cargo_capacity: string;
+    consumables: string;
+    pilots: string[];
+    length: string;
+    max_atmosphering_speed: string;
+    hyperdrive_rating: string;
+    MGLT: string;
+  }
+  
+  const StarshipDetails: React.FC = () => {
+    const { id } = useParams();
+    const [starship, setStarship] = useState<Starship | null>(null);
+  
+    useEffect(() => {
+      axios.get(`https://swapi.dev/api/starships/${id}/`)
+        .then(response => {
+          setStarship(response.data);
+        })
+        .catch(error => console.log(error));
+    }, [id]);
+  
+    if (!starship) return <p>Loading...</p>;
+  
+    return (
+      <div>
+        <h2>{starship.name}</h2>
+        <p>Model: {starship.model}</p>
+        <p>Starship Class: {starship.starship_class}</p>
+        <p>Manufacturer: {starship.manufacturer}</p>
+        <p>Cost: {starship.cost_in_credits}</p>
+        <p>Crew: {starship.crew}</p>
+        <p>Passenger Capacity: {starship.passengers}</p>
+        <p>Cargo Capacity: {starship.cargo_capacity}</p>
+        <p>Consumables: {starship.consumables}</p>
+        <p>Length: {starship.length}</p>
+        <p>Maximum Atmosphering Speed: {starship.max_atmosphering_speed}</p>
+        <p>Hyperdrive Rating: {starship.hyperdrive_rating}</p>
+        <p>Maximum Speed in Realspace: {starship.MGLT}</p>
+        <img src={`https://starwars-visualguide.com/assets/img/starships/${id}.jpg`} alt={starship.name} />
+      </div>
+    );
+  };
+  
+  export default StarshipDetails;
--- src/components/Starships.tsx
+++ src/components/Starships.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import "./Starships.css";
@@ -11,13 +11,40 @@ type Starship = {
 
 const Starships: React.FC = () => {
   const [starships, setStarships] = useState<Starship[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const loadingRef = useRef(loading);
+  loadingRef.current = loading;
+
+  const loadStarships = async (page: number) => {
+    setLoading(true);
+    const response = await axios.get(`https://swapi.dev/api/starships/?page=${page}`);
+    setStarships(prevStarships => {
+      const newStarships = response.data.results.filter((newStarship: Starship) =>
+        !prevStarships.some((prevStarship: Starship) => prevStarship.url === newStarship.url)
+      );
+      return [...prevStarships, ...newStarships];
+    });
+    setLoading(false);
+  };
 
   useEffect(() => {
-    axios.get('https://swapi.dev/api/starships/')
-      .then(response => {
-        setStarships(response.data.results);
-      })
-      .catch(error => console.log(error));
+    if (!loadingRef.current) {
+      loadStarships(page);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        if (!loadingRef.current) {
+          setPage(prevPage => prevPage + 1);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
@@ -25,8 +52,8 @@ const Starships: React.FC = () => {
       {starships.map(starship => (
         <Link key={starship.url} to={`/starships/${starship.url.split('/')[5]}`}>
           <div className="naus">
-          <h2>{starship.name}</h2>
-          <p>{starship.model}</p>
+            <h2>{starship.name}</h2>
+            <p>{starship.model}</p>
           </div>
         </Link>
       ))}
